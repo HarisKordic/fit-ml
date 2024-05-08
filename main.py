3c0886ee055a23ba190cb9fb53801403d83c3fd3
Haris Kordic <kordicharis18@gmail.com>
2024-05-08 22:04:24 +0200

    fix(main.py): reading model subjects

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import pandas as pd
 
 # Define the file paths
 input_file_path = r'data\raw_data.csv'
+model_subjects_path = r'data\model_subjects.csv'
 output_file_path = r'data\filtered_model_data.csv'
 
 # Read the CSV file with a specific encoding
 df = pd.read_csv(input_file_path, delimiter=';', encoding='latin1')
 
-# Define the filter values
-filter_values = ['P-175', 'P-176', 'P-177', 'P-150', 'P-157']
+# Read the filter values as a single row and split them into individual values
+with open(model_subjects_path, 'r') as file:
+    filter_values = file.read().strip().split(',')
 
 # Filter the 'Predmet' column
 filtered_df = df[df['Predmet'].isin(filter_values)]
